2edf166711d0a58c67f61d1e6af556be03b0fa5b
kbullard <kari@projectcost.net>
2020-02-12 14:09:54 +0800

    updated angular.json schematics, created recipe module with base components

---
+++ src/app/recipes/recipe-detail/recipe-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dav-cooks-recipe-detail',
+  templateUrl: './recipe-detail.component.html',
+  styleUrls: ['./recipe-detail.component.scss']
+})
+export class RecipeDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/recipes/recipes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'dav-cooks-recipes',
+  templateUrl: './recipes.component.html',
+  styleUrls: ['./recipes.component.scss']
+})
+export class RecipesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
